3e0b7c2356ee0971d84119af0d00bc0b1de964e7
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-06-17 21:44:09 +0200

    Initial upload #2

---
+++ src/Utils/logger.js
@@ -0,0 +1,9 @@
+/** @author KinashServerMCBE */
+/** @author andriycraft */
+
+class Logger {
+   constructor() {}
+   info (message) {
+   	console.log(`Info - ${message}`)
+   }
+}
--- src/server.js
+++ src/server.js
@@ -1,22 +1,29 @@
 /** @author KinashServerMCBE */
 /** @author andriycraft */
 
-const { RakNetServer, InternetAddress, Frame, ReliabilityTool } = require("bbmc-raknet");
+const { RakNetServer, InternetAddress } = require("bbmc-raknet");
 const Data = require('./data')
+const config = require('../../config.json')
+//const Logger = require('')
 
-class server {
-    constructor() {
-        this.raknet = new RakNetServer(
-            new InternetAddress(config.host, config.port, Data.bind_version), Data.magicnumber
-        );
-        this.handle()
-    }
 
-    handle() {
-        let interval = setInterval(() => {
-            if (this.raknet.isRunning === true) {
-                this.raknet.message = `MCPE;MOTD;555;1.19.0;69;420;${raknet.serverGUID.toString()};`;
+const raknet = new RakNetServer(
+	new InternetAddress(config.host, config.port, Data.bind_version), Data.magicnumber
+);
+console.log('ha')
+
+function handle() {
+	console.log('handle')
+        setInterval(() => {
+            if (raknet.isRunning === true) {
+		let ver = "1.19.0"
+		let protocol = 527
+                raknet.message = `MCPE;idk modd;${protocol};${ver};0;5;${raknet.serverGUID.toString()};`;
             } 
         });
-    }
+	raknet.on('connect', (connection) => {
+			console.log('connect')
+		});
 }
+
+handle()
