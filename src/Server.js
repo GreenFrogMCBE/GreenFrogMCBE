9ab0406468d4c558406630dee8836c9bdea879c8
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-06-18 14:41:40 +0200

    Logger.js

--- src/Server.js
+++ src/Server.js
@@ -4,14 +4,22 @@
 const { RakNetServer, InternetAddress } = require("bbmc-raknet");
 const Data = require('./Utils/Data')
 const Logger = require('./Utils/Logger')
+
+Logger.info('Loading server...')
+Logger.info('Loading config.json')
+try {
+	const Config = require('../../config.json')
+} catch (e) {
+	Logger.info('Failed to load config.json')
+	process.exit(-1)
+}
 const Config = require('../../config.json')
-
-
+Logger.info('Creating raknet server...')
 const raknet = new RakNetServer(new InternetAddress(Config.host, Config.port, Data.bind_version), Data.magicnumber);
-console.log('ha')
+
 
 function handle() {
-	console.log('handle')
+	Logger.info('Raknet handler started')
 	setInterval(() => {
 		if (raknet.isRunning) {
 			raknet.message = `MCPE;${Config.motd};${Data.protocol};${Data.version};0;${Data.maxplayers};${raknet.serverGUID.toString()};`;
