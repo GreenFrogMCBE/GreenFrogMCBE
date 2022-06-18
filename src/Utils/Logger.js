ce7384c71e2f2b7a225e7e15448e7d9ae8702e73
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-06-18 14:32:01 +0200

    commit

--- src/Utils/Logger.js
+++ src/Utils/Logger.js
@@ -1,9 +1,20 @@
 /** @author KinashServerMCBE */
 /** @author andriycraft */
 
+const Color = require('./ConsoleColors')
+
 class Logger {
    constructor() {}
+
    info (message) {
-   	console.log(`Info - ${message}`)
+   	console.log(`${Color.cyan} Info ${Color.white} - ${message}`)
+   }
+
+   warn (message) {
+   	console.log(`${Color.yellow} Warning ${Color.white} - ${message}`)
+   }
+
+   error (message) {
+   	console.log(`${Color.red} Error ${Color.white} - ${message}`)
    }
 }
