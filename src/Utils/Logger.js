a74425af0dc5416d87eb01bdb30283374b6bbb93
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-06-18 21:44:41 +0200

    Better logger

--- src/Utils/Logger.js
+++ src/Utils/Logger.js
@@ -7,16 +7,21 @@ class Logger {
 
    constructor () { }
 
+   getDate() {
+	let d = new Date()
+  	return `${Color.blue}${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`
+   }
+
    info (message) {
-   	console.log(`${Color.cyan} Info ${Color.white} - ${message}`)
+   	console.log(`${this.getDate()}${Color.cyan} Info${Color.white} - ${message}`)
    }
 
    warn (message) {
-   	console.log(`${Color.yellow} Warning ${Color.white} - ${message}`)
+   	console.log(`${this.getDate()}${Color.yellow} Warning${Color.white} - ${message}`)
    }
 
    error (message) {
-   	console.log(`${Color.red} Error ${Color.white} - ${message}`)
+   	console.log(`${this.getDate()}${Color.red} Error${Color.white} - ${message}`)
    }
 
 }
