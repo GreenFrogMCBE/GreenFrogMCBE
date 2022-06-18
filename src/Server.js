72f0fc4ddd55ff551e8dad58711c79c84c72b52b
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-06-18 14:33:28 +0200

    remove this thing

--- src/Server.js
+++ src/Server.js
@@ -7,14 +7,14 @@ const Logger = require('./Utils/Logger')
 const Config = require('../../config.json')
 
 
-const raknet = new RakNetServer(new InternetAddress(config.host, config.port, Data.bind_version), Data.magicnumber);
+const raknet = new RakNetServer(new InternetAddress(Config.host, Config.port, Data.bind_version), Data.magicnumber);
 console.log('ha')
 
 function handle() {
 	console.log('handle')
 	setInterval(() => {
 		if (raknet.isRunning) {
-			raknet.message = `${config.edition};${config.motd};${Data.protocol};${Data.version};0;${Data.maxplayers};${raknet.serverGUID.toString()};`;
+			raknet.message = `${Config.edition};${Config.motd};${Data.protocol};${Data.version};0;${Data.maxplayers};${raknet.serverGUID.toString()};`;
 		}
 	});
 	raknet.on('connect', (connection) => {
