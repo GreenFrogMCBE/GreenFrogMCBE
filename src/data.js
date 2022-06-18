806ddcda808bd0695f626fe2838745c0ba1536d4
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-06-18 14:20:49 +0200

    stuff

--- src/data.js
+++ src/data.js
@@ -4,4 +4,6 @@
 class data {
     static magic_number = 5
     static bind_version = 4
+    static protocol = 527;
+    static version = "1.19.0"
 }
--- src/server.js
+++ src/server.js
@@ -7,23 +7,19 @@ const config = require('../../config.json')
 //const Logger = require('')
 
 
-const raknet = new RakNetServer(
-	new InternetAddress(config.host, config.port, Data.bind_version), Data.magicnumber
-);
+const raknet = new RakNetServer(new InternetAddress(config.host, config.port, Data.bind_version), Data.magicnumber);
 console.log('ha')
 
 function handle() {
 	console.log('handle')
-        setInterval(() => {
-            if (raknet.isRunning === true) {
-		let ver = "1.19.0"
-		let protocol = 527
-                raknet.message = `MCPE;idk modd;${protocol};${ver};0;5;${raknet.serverGUID.toString()};`;
-            } 
-        });
+	setInterval(() => {
+		if (raknet.isRunning === true) {
+			raknet.message = `MCPE;Motd;${Data.protocol};${Data.version};0;5;${raknet.serverGUID.toString()};`;
+		}
+	});
 	raknet.on('connect', (connection) => {
-			console.log('connect')
-		});
+		console.log('connect')
+	});
 }
 
 handle()
