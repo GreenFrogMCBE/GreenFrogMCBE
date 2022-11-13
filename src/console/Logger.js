c4f2fc7d8cf9a26693590844c5d2f6163fdba0f2
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-11-13 12:38:06 +0100

    new logging system

--- src/console/Logger.js
+++ src/console/Logger.js
@@ -2,9 +2,22 @@ class Logger {
     constructor() { }
 
     log (message, type = 'info') {
-        console.log(`${type}: ${message}`)
+        switch (type) {
+            case 'info':
+                console.log(`\x1b[34mINFO\x1b[0m | ${message}`)
+                break
+            case 'warning':
+            case 'warn':
+                console.log(`\x1b[33mWARNING\x1b[0m | ${message}`)
+                break
+            case 'error':
+            case 'err':
+                console.log(`\x1b[31mERROR\x1b[0m | ${message}`)
+                break
+            default:
+                throw new Error(`Invalid log type: ${type}`)
+        }
     }
-
 }
 
 module.exports = Logger;
