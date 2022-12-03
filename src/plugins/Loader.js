6410ecc00a1388db9446664d4f34e9492788278f
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-12-03 14:23:30 +0000

    im dumb :skull:

--- src/plugins/Loader.js
+++ src/plugins/Loader.js
@@ -8,11 +8,11 @@ class Loader {
     loadPlugins() {
         try {
             Logger.prototype.log('Loading plugins...')
-            fs.readdirSync("../plugins")
+            fs.readdirSync("./plugins")
         } catch (e) {
             Logger.prototype.log('Plugins folder not found. Creating it...')
             try {
-                fs.mkdirSync("../plugins", { recursive: true })
+                fs.mkdirSync("./plugins", { recursive: true })
                 Logger.prototype.log('Plugins folder created')
             } catch (e) {
                 Logger.prototype.log(`Failed to create plugins folder, this is a fatal error, shutting down: ${e}`, 'error')
