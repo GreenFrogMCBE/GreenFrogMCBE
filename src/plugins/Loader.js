b849644e57b3022ab92f8bad88e5c4290673a6e5
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-12-03 14:22:31 +0000

    added checking for plugins folder

--- src/plugins/Loader.js
+++ src/plugins/Loader.js
@@ -1,5 +1,25 @@
+const fs = require('fs')
+const Logger = require('../console/Logger')
+
 class Loader {
-    
+
+    constructor() { }
+
+    loadPlugins() {
+        try {
+            Logger.prototype.log('Loading plugins...')
+            fs.readdirSync("../plugins")
+        } catch (e) {
+            Logger.prototype.log('Plugins folder not found. Creating it...')
+            try {
+                fs.mkdirSync("../plugins", { recursive: true })
+                Logger.prototype.log('Plugins folder created')
+            } catch (e) {
+                Logger.prototype.log(`Failed to create plugins folder, this is a fatal error, shutting down: ${e}`, 'error')
+            }
+        }
+    }
+
 }
 
 module.exports = Loader;
