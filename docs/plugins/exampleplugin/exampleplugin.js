742d412ee6845dc567bb83cf976ad8bb165fdf1d
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-12-17 21:30:17 +0100

    Update exampleplugin.js

--- docs/plugins/exampleplugin/exampleplugin.js
+++ docs/plugins/exampleplugin/exampleplugin.js
@@ -2,52 +2,51 @@
 // This plugin contains all events and descriptions, so you can use this plugin as a docs
 
 class ExamplePlugin {
-    constructor() {}
+    constructor () {}
 
-    getName() {
+    getName () {
         return "Example plugin" // Your plugin name
     }
 
-    onLoad() {
+    onLoad () {
         // This code executes when your plugin is loaded
         console.log(`I was loaded`)
     }
 
-    onJoin(server, client) {
+    onJoin (server, client) {
         // This code executes when player joined
         console.log(`A wild player appeared!`)
     }
 
-    onResourcePackInfoSent(server, client) { }
-    onPlayerHasNoResourcePacksInstalled(server, client) { }
-    onResourcePacksRefused(server, client) { }
-    onPlayerHaveAllPacks(server, client) { }
-    onResourcePacksCompleted(server, client) { }
+    onResourcePackInfoSent (server, client) { }
+    onPlayerHasNoResourcePacksInstalled (server, client) { }
+    onResourcePacksRefused (server, client) { }
+    onPlayerHaveAllPacks (server, client) { }
+    onResourcePacksCompleted (server, client) { }
 
-    onKick(server, client, msg) {
+    onKick (server, client, msg) {
         // This code executes when player is kicked
         console.log(`Oh no! Player got kicked: ${msg}`)
     }
 
-    onPlayerSpawn(server, client) {
+    onPlayerSpawn (server, client) {
         // This code executes when player is spawned (this event triggers after onJoin() event)
     }
 
-    onChat(server, client, msg, fullmsg) {
+    onChat (server, client, msg, fullmsg) {
         // This code executes when player uses chat
         console.log(`Client used chat: ${msg}`)
     }
 
-    onCommand(server, client, command) {
+    onCommand (server, client, command) {
         // This code executes when player executes a command
         console.log(`Client executed this command: ${command}`)
     }
 
-    onInternalServerError(server, client, error) {
+    onInternalServerError (server, client, error) {
         // This code executes when there is an server error
         console.log(`Oh no! There was an server error: ${error}`)
     }
-
 }
 
 module.exports = ExamplePlugin;
