6a189f1a839bfb58ca532f9457d088e78f6fe12e
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-12-17 21:48:26 +0100

    Create DonationReminder.js

---
+++ plugins/DonationReminder.js
@@ -0,0 +1,27 @@
+class DonationReminder {
+    constructor() {}
+
+    getName() {
+        return "DonationReminder"
+    }
+
+    onLoad() {
+        console.log(`\x1b[34mINFO\x1b[0m | Hi there! If you want to support GreenFrogMCBE you can donate at: https://www.paypal.com/donate/?hosted_button_id=EMT6MHNNL3KBQ`)
+        console.log(`\x1b[34mINFO\x1b[0m | You will also be listed in our Discord server!`)
+        console.log(`\x1b[34mINFO\x1b[0m | If you do not want to support us just delete the DonationPlugin.js from plugins folder`)
+    }
+
+    onJoin(server, client) { }
+    onResourcePackInfoSent(server, client) { }
+    onPlayerHasNoResourcePacksInstalled(server, client) { }
+    onResourcePacksRefused(server, client) { }
+    onPlayerHaveAllPacks(server, client) { }
+    onResourcePacksCompleted(server, client) { }
+    onKick(server, client, msg) { }
+    onPlayerSpawn(server, client) { }
+    onChat(server, client, msg, fullmsg) { }
+    onCommand(server, client, command) { }
+    onInternalServerError(server, client, error) { }
+}
+
+module.exports = DonationReminder;
