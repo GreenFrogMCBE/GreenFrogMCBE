0494baf58b6be3b156681bce990b3ca67de477e3
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-12-17 23:05:55 +0100

    Update Logger.js

--- src/console/Logger.js
+++ src/console/Logger.js
@@ -1,4 +1,5 @@
 const config = require('../../config.json')
+const lang = require(`../../lang/${config.lang}.json`)
 
 class Logger {
     constructor() { }
@@ -6,19 +7,19 @@ class Logger {
     log(message, type = 'info') {
         switch (type) {
             case 'info':
-                console.log(`\x1b[34mINFO\x1b[0m | ${message}`)
+                console.log(`\x1b[34m${lang.info.toUpperCase()}\x1b[0m | ${message}`)
                 break
             case 'warning':
             case 'warn':
-                console.log(`\x1b[33mWARNING\x1b[0m | ${message}`)
+                console.log(`\x1b[33m${lang.warning.toUpperCase()}\x1b[0m | ${message}`)
                 break
             case 'error':
             case 'err':
-                console.log(`\x1b[31mERROR\x1b[0m | ${message}`)
+                console.log(`\x1b[31m${lang.error.toUpperCase()}\x1b[0m | ${message}`)
                 break
             case 'debug':
                 if (config.debug) {
-                    console.log(`DEBUG | ${message}`)
+                    console.log(`${lang.debug.toUpperCase()} | ${message}`)
                 }
                 break
             default:
