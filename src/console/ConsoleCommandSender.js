e4f95795b8fd41dacd7a785e6e3ce8e6d447c91b
andriycraft <kinashtesttest@gmail.com>
2022-12-18 11:30:05 +0100

    An attempt to fix bugs

--- src/console/ConsoleCommandSender.js
+++ src/console/ConsoleCommandSender.js
@@ -26,6 +26,7 @@ class ConsoleCommandSender {
                     }
                 });
             });
+            console.log(ServerInfo.prototype.getLang())
             if (data.startsWith(`${ServerInfo.prototype.getLang().command_time} `)) {
                 if (!ServerInfo.prototype.getCommands().console_time) { Logger.prototype.log(ServerInfo.prototype.getLang().unknown_command); return }
                 const time = parseInt(data.split(" ")[1])
