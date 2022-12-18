bb2c86a2a9f5f27a7d618b0e6419b75273aea11b
andriycraft <kinashtesttest@gmail.com>
2022-12-18 11:31:06 +0100

    An attempt to fix bugs

--- src/console/ConsoleCommandSender.js
+++ src/console/ConsoleCommandSender.js
@@ -27,16 +27,16 @@ class ConsoleCommandSender {
                     }
                 });
             });
-            if (data.startsWith(`${ServerInfo.prototype.getLang().command_time} `)) {
-                if (!ServerInfo.prototype.getCommands().console_time) { Logger.prototype.log(ServerInfo.prototype.getLang().unknown_command); return }
+            if (data.startsWith(`${lang.command_time} `)) {
+                if (!ServerInfo.prototype.getCommands().console_time) { Logger.prototype.log(lang.unknown_command); return }
                 const time = parseInt(data.split(" ")[1])
                 if (time === NaN) {
-                    Logger.prototype.log(ServerInfo.prototype.getLang().invalid_time)
+                    Logger.prototype.log(lang.invalid_time)
                     return
                 }
 
                 if (ServerInfo.prototype.getPlayers() === undefined) {
-                    Logger.prototype.log(ServerInfo.prototype.getLang().no_players_online)
+                    Logger.prototype.log(lang.no_players_online)
                     return
                 }
 
@@ -45,26 +45,26 @@ class ConsoleCommandSender {
                     client.write('set_time', { time: time })
                 }
 
-                Logger.prototype.log(ServerInfo.prototype.getLang().time_updated)
+                Logger.prototype.log(lang.time_updated)
                 return
 
             }
 
-            if (data.startsWith(`${ServerInfo.prototype.getLang().command_say} `)) {
-                if (!ServerInfo.prototype.getCommands().console_say) { Logger.prototype.log(ServerInfo.prototype.getLang().unknown_command); return }
+            if (data.startsWith(`${lang.command_say} `)) {
+                if (!ServerInfo.prototype.getCommands().console_say) { Logger.prototype.log(lang.unknown_command); return }
 
                 const msg = data.split(" ")[1]
                 if (msg.length < 1) {
-                    Logger.prototype.log(ServerInfo.prototype.getLang().empty_message)
+                    Logger.prototype.log(lang.empty_message)
                     return
                 }
 
                 if (ServerInfo.prototype.getPlayers() === undefined) {
-                    Logger.prototype.log(ServerInfo.prototype.getLang().no_players_online)
+                    Logger.prototype.log(lang.no_players_online)
                     return
                 }
 
-                let msg1 = ServerInfo.prototype.getLang().chat__saycommand_format.replace(`%message%`, msg)
+                let msg1 = lang.chat__saycommand_format.replace(`%message%`, msg)
 
                 for (let i = 1; i < ServerInfo.prototype.getPlayers().length; i++) {
                     let client = ServerInfo.prototype.getPlayers()[i]
@@ -84,8 +84,8 @@ class ConsoleCommandSender {
                 return
             }
 
-            if (data.startsWith(`${ServerInfo.prototype.getLang().command_kick} `)) {
-                if (!ServerInfo.prototype.getCommands().console_kick) { Logger.prototype.log(ServerInfo.prototype.getLang().unknown_command); return }
+            if (data.startsWith(`${lang.command_kick} `)) {
+                if (!ServerInfo.prototype.getCommands().console_kick) { Logger.prototype.log(lang.unknown_command); return }
 
                 const player = data.split(" ")[1]
                 let reason = ""
@@ -99,73 +99,73 @@ class ConsoleCommandSender {
                 }
 
                 if (!reason) {
-                    reason = `${ServerInfo.prototype.getLang().no_reason}`
+                    reason = `${lang.no_reason}`
                 }
 
                 if (ServerInfo.prototype.getPlayers() === undefined) {
-                    Logger.prototype.log(ServerInfo.prototype.getLang().no_players_online)
+                    Logger.prototype.log(lang.no_players_online)
                     return
                 }
 
                 for (let i = 0; i < ServerInfo.prototype.getPlayers().length; i++) {
                     let client = ServerInfo.prototype.getPlayers()[i]
                     if (client.getUserData().displayName == player) {
-                        client.disconnect(ServerInfo.prototype.getLang().kicked_prefix + reason)
-                        Logger.prototype.log(ServerInfo.prototype.getLang().kicked_consolemsg.replace('%player%', player).replace('%reason%', reason), 'info')
+                        client.disconnect(lang.kicked_prefix + reason)
+                        Logger.prototype.log(lang.kicked_consolemsg.replace('%player%', player).replace('%reason%', reason), 'info')
                         playeroffline = false
                     }
                 }
 
                 if (playeroffline) {
-                    Logger.prototype.log(ServerInfo.prototype.getLang().playeroffline, 'info')
+                    Logger.prototype.log(lang.playeroffline, 'info')
                 }
 
                 return
             }
             switch (data.toLowerCase()) {
-                case ServerInfo.prototype.getLang().empty:
+                case lang.empty:
                     break
-                case ServerInfo.prototype.getLang().command_shutdown:
-                case ServerInfo.prototype.getLang().command_stop:
-                    if (!ServerInfo.prototype.getCommands().console_stop) { Logger.prototype.log(ServerInfo.prototype.getLang().unknown_command); return; }
-                    Logger.prototype.log(ServerInfo.prototype.getLang().stopping_server, 'info')
+                case lang.command_shutdown:
+                case lang.command_stop:
+                    if (!ServerInfo.prototype.getCommands().console_stop) { Logger.prototype.log(lang.unknown_command); return; }
+                    Logger.prototype.log(lang.stopping_server, 'info')
                     try {
                         for (let i = 0; i < ServerInfo.prototype.getPlayers().length; i++) {
-                            ServerInfo.prototype.getPlayers()[i].disconnect(ServerInfo.prototype.getLang().kick__servershutdown)
+                            ServerInfo.prototype.getPlayers()[i].disconnect(lang.kick__servershutdown)
                         }
                     } catch (e) { }
                     setTimeout(() => {
                         process.exit(0)
                     }, 1000) // give some time for the server to disconnect clients
                     break
-                case ServerInfo.prototype.getLang().command_kick:
-                    if (!ServerInfo.prototype.getCommands().console_kick) { Logger.prototype.log(ServerInfo.prototype.getLang().unknown_command); return; }
-                    Logger.prototype.log(ServerInfo.prototype.getLang().command_usage_kick, 'info')
+                case lang.command_kick:
+                    if (!ServerInfo.prototype.getCommands().console_kick) { Logger.prototype.log(lang.unknown_command); return; }
+                    Logger.prototype.log(lang.command_usage_kick, 'info')
                     break;
-                case ServerInfo.prototype.getLang().command_ver:
-                case ServerInfo.prototype.getLang().command_version:
-                    if (!ServerInfo.prototype.getCommands().console_version) { Logger.prototype.log(ServerInfo.prototype.getLang().unknown_command); return; }
-                    Logger.prototype.log(ServerInfo.prototype.getLang().command_ver_info, 'info')
+                case lang.command_ver:
+                case lang.command_version:
+                    if (!ServerInfo.prototype.getCommands().console_version) { Logger.prototype.log(lang.unknown_command); return; }
+                    Logger.prototype.log(lang.command_ver_info, 'info')
                     break;
-                case ServerInfo.prototype.getLang().command_time:
-                    if (!ServerInfo.prototype.getCommands().console_time) { Logger.prototype.log(ServerInfo.prototype.getLang().unknown_command); return; }
-                    Logger.prototype.log(ServerInfo.prototype.getLang().command_usage_time, 'info')
+                case lang.command_time:
+                    if (!ServerInfo.prototype.getCommands().console_time) { Logger.prototype.log(lang.unknown_command); return; }
+                    Logger.prototype.log(lang.command_usage_time, 'info')
                     break;
                 case '?':
-                case ServerInfo.prototype.getLang().command_help:
-                    if (!ServerInfo.prototype.getCommands().console_command_help) { Logger.prototype.log(ServerInfo.prototype.getLang().unknown_command); return; }
-                    Logger.prototype.log(ServerInfo.prototype.getLang().commandlist);
-                    if (ServerInfo.prototype.getCommands().console_command_kick) { Logger.prototype.log(ServerInfo.prototype.getLang().kick_help); }
-                    if (ServerInfo.prototype.getCommands().console_command_stop) { Logger.prototype.log(ServerInfo.prototype.getLang().shutdown_help); }
-                    if (ServerInfo.prototype.getCommands().console_command_stop) { Logger.prototype.log(ServerInfo.prototype.getLang().stop_help); }
-                    if (ServerInfo.prototype.getCommands().console_command_help) { Logger.prototype.log(ServerInfo.prototype.getLang().help_help); }
-                    if (ServerInfo.prototype.getCommands().console_command_help) { Logger.prototype.log(ServerInfo.prototype.getLang().qm_help) }
-                    if (ServerInfo.prototype.getCommands().console_command_version) { Logger.prototype.log(ServerInfo.prototype.getLang().version_help); }
-                    if (ServerInfo.prototype.getCommands().console_command_version) { Logger.prototype.log(ServerInfo.prototype.getLang().ver_help); }
-                    if (ServerInfo.prototype.getCommands().console_command_time) { Logger.prototype.log(ServerInfo.prototype.getLang().time_help); }
+                case lang.command_help:
+                    if (!ServerInfo.prototype.getCommands().console_command_help) { Logger.prototype.log(lang.unknown_command); return; }
+                    Logger.prototype.log(lang.commandlist);
+                    if (ServerInfo.prototype.getCommands().console_command_kick) { Logger.prototype.log(lang.kick_help); }
+                    if (ServerInfo.prototype.getCommands().console_command_stop) { Logger.prototype.log(lang.shutdown_help); }
+                    if (ServerInfo.prototype.getCommands().console_command_stop) { Logger.prototype.log(lang.stop_help); }
+                    if (ServerInfo.prototype.getCommands().console_command_help) { Logger.prototype.log(lang.help_help); }
+                    if (ServerInfo.prototype.getCommands().console_command_help) { Logger.prototype.log(lang.qm_help) }
+                    if (ServerInfo.prototype.getCommands().console_command_version) { Logger.prototype.log(lang.version_help); }
+                    if (ServerInfo.prototype.getCommands().console_command_version) { Logger.prototype.log(lang.ver_help); }
+                    if (ServerInfo.prototype.getCommands().console_command_time) { Logger.prototype.log(lang.time_help); }
                     break;
                 default:
-                    Logger.prototype.log(ServerInfo.prototype.getLang().unknown_command)
+                    Logger.prototype.log(lang.unknown_command)
                     break
             }
             r.prompt(true)
