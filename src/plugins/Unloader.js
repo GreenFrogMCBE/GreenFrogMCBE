553c45717fa06f3aeb4fb330c3df1c5c3771f469
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-12-30 00:18:11 +0100

    Replaced "sec" with "seconds"

--- src/plugins/Unloader.js
+++ src/plugins/Unloader.js
@@ -6,15 +6,15 @@ class Unloader {
     constructor() {}
 
     shutdown() {
-        // TODO: The if u have potato hosting then the plugins may not shutdown correctly. I warned you
         fs.readdir("./plugins", (err, plugins) => {
             Logger.prototype.log('Shutting down plugins...')
-            Logger.prototype.log('The server has 10 sec to shutdown the plugins. If the servers fails to do it in time - there may be some issues with plugin data', 'warning')
+            Logger.prototype.log('The server has 10 seconds to shutdown the plugins. If the servers fails to do it in time - there may be some issues with plugin data', 'warning')
             plugins.forEach(plugin => {
                     if (require(`../../plugins/${plugin}`).prototype.getName() === "") {
                         throw new Error(`Failed to shutdown plugin ${plugin}. No plugin name!`)
                     }
 
+                    // TODO: Somehow use await
                     require(`../../plugins/${plugin}`).prototype.onShutdown()
             });
             Logger.prototype.log('Done shutting down plugins!')
