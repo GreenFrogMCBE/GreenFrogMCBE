498cb9838180a75509bf204496f62dfde6c560f1
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-01-04 22:02:22 +0100

    Update KickPlayer.js

--- src/player/KickPlayer.js
+++ src/player/KickPlayer.js
@@ -3,7 +3,7 @@ class KickPlayer {
     constructor() { }
 
     kick(client, reason = 'You were disconnected') {
-        if (!client) throw new Error("kick(): No client!")
+        if (!client) throw new Error("kick(client): No client!")
         
         client.kick(reason)
     }
