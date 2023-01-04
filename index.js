619118c36eaca1a879aaf8f60d3b3aed6b347500
andriycraft <kinashtesttest@gmail.com>
2023-01-04 22:38:20 +0100

    Automatically run "CheckNetIsolation LoopbackExempt -a -n=Microsoft.MinecraftUWP_8wekyb3d8bbwe" at start

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+const { exec } = require('node:child_process');
+if (process.platform === 'win32') {
+    exec(`powershell /c CheckNetIsolation LoopbackExempt -a -n=Microsoft.MinecraftUWP_8wekyb3d8bbwe`, () => {});
+}
+
 require('./src/Server.js')
