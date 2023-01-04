8395ba09e6c60f406c3d9b708750a0d69baf49ee
andriycraft <kinashtesttest@gmail.com>
2023-01-04 22:42:25 +0100

    Automatically install bedrock-protocol if it is not installed

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const { exec } = require('node:child_process');
+
+async function loadBP() {
+    try {
+        require('bedrock-protocol')
+    } catch (e) {
+        console.log(`Error: No bedrock-protocol found! Installing...`)
+        await exec(`npm i bedrock-protocol`, () => { });
+    }
+}
+
 if (process.platform === 'win32') {
-    exec(`powershell /c CheckNetIsolation LoopbackExempt -a -n=Microsoft.MinecraftUWP_8wekyb3d8bbwe`, () => {});
+    exec(`powershell /c CheckNetIsolation LoopbackExempt -a -n=Microsoft.MinecraftUWP_8wekyb3d8bbwe`, () => { });
 }
 
 require('./src/Server.js')
