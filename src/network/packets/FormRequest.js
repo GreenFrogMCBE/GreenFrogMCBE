4befca1e73de32598ce982829fdfdec2c7b60f88
andriycraft <kinashtesttest@gmail.com>
2023-02-05 15:36:44 +0100

    bruh

--- src/network/packets/FormRequest.js
+++ src/network/packets/FormRequest.js
@@ -10,12 +10,12 @@
  * Copyright 2023 andriycraft
  * Github: https://github.com/andriycraft/GreenFrogMCBE
  */
-/* It's a class that sends a text packet to the client */
+
 class FormRequest extends require("./Packet") {
   /**
- * It returns the packet name
- * @returns Packet name
- */
+  * It returns the packet name
+  * @returns Packet name
+  */
   name() {
     return "modal_form_request";
   }
