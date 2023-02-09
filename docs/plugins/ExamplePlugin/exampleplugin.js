16a82468a6340000a5eebdbe0477aba9496dad50
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-02-09 16:20:41 +0100

    Update exampleplugin.js

--- docs/plugins/ExamplePlugin/exampleplugin.js
+++ docs/plugins/ExamplePlugin/exampleplugin.js
@@ -33,31 +33,31 @@ module.exports = {
     Logger.log(`${this.name} > Good bye!`);
   },
 
-  onJoin(server, client) {
+  onJoin(server, player) {
     // This code executes when player joined
   },
 
   // REMEMBER: You can just remove events that you don't use
 
-  onLeave(server, client) {
+  onLeave(server, player) {
     // This code executes when player left the server
   },
 
-  onPlayerHasNoResourcePacksInstalled(server, client) {},
-  onResourcePacksRefused(server, client) {},
-  onPlayerHaveAllPacks(server, client) {},
-  onResourcePacksCompleted(server, client) {},
+  onPlayerHasNoResourcePacksInstalled(server, player) {},
+  onResourcePacksRefused(server, player) {},
+  onPlayerHaveAllPacks(server, player) {},
+  onResourcePacksCompleted(server, player) {},
 
-  onKick(server, client, msg) {
+  onKick(server, player, msg) {
     // This code executes when player is kicked
   },
 
-  onPlayerSpawn(server, client) {
+  onPlayerSpawn(server, player) {
     // Registers a command
     const cmdmanager = new CommandManager();
-    cmdmanager.addCommand(client, "testcommand", "This is my first command!");
+    cmdmanager.addCommand(player, "testcommand", "This is my first command!");
     cmdmanager.addCommand(
-      client,
+      player,
       "stopserver",
       "Stop server command that is registered by the example plugin"
     );
@@ -66,54 +66,54 @@ module.exports = {
     // This code executes when player is spawned (this event executes after onJoin() event)
   },
 
-  onChat(server, client, message) {
-    client.sendMessage(client, "Your just sent a chat message: " + message);
+  onChat(server, player, message) {
+    player.sendMessage(player, "Your just sent a chat message: " + message);
     // This code executes when player uses chat
   },
 
-  onCommand(server, client, command) {
+  onCommand(server, player, command) {
     switch (command.toLowerCase()) {
       case "/testcommand":
-        // client.username returns the client's username
-        // client.ip returns the client's ip without port
-        // client.port returns the client's connection port
-        // client.fullip returns client's ip and port
-        // client.gamemode returns client's gamemode
-        // client.offline checks if the client is online or not
-        // client.op returns the client's op status
-        // client.permlevel returns the client's permission level
-        client.sendMessage(`Hi ${client.username}. Your IP is: ${client.ip}`); // This code sends message TO client
-        client.chat(`This message was sent by ${this.name}`); // This code sends message AS A client
-        client.setGamemode(GameMode.CREATIVE); // This updates the client gamemode. Valid gamemodes are: "creative", "survival", "adventure", "spectator" or "fallback"
+        // player.username returns the player's username
+        // player.ip returns the player's ip without port
+        // player.port returns the player's connection port
+        // player.fullip returns player's ip and port
+        // player.gamemode returns player's gamemode
+        // player.offline checks if the player is online or not
+        // player.op returns the player's op status
+        // player.permlevel returns the player's permission level
+        player.sendMessage(`Hi ${player.username}. Your IP is: ${player.ip}`); // This code sends message TO player
+        player.chat(`This message was sent by ${this.name}`); // This code sends message AS A player
+        player.setGamemode(GameMode.CREATIVE); // This updates the player gamemode. Valid gamemodes are: "creative", "survival", "adventure", "spectator" or "fallback"
 
         const Toast = new ToastManager();
         Toast.title = "Hello, world";
         Toast.message = "This is an example of a Toast";
-        Toast.send(client);
+        Toast.send(player);
 
         const form = new Form();
         form.buttons = [{ text: "Button 1" }];
         form.content = "Hello, world";
         form.title = "Hello, world (title)";
         form.type = "form";
-        form.send(client);
+        form.send(player);
 
         //              ^ title ^ toast description/body
-        client.setTime(17000); // Updates the client time
+        player.setTime(17000); // Updates the player time
         setTimeout(() => {
-          if (!client.offline) {
-            // Make sure to check if the client is still online after doing setTimeout() that uses client API in production plugins
-            client.transfer("172.0.0.1", 19132); // Moves player to another server
+          if (!player.offline) {
+            // Make sure to check if the player is still online after doing setTimeout() that uses player API in production plugins
+            player.transfer("172.0.0.1", 19132); // Moves player to another server
             //              ^ ip         ^ port
           }
         }, 30000);
 
         // ADVANCED API
-        // client.write(packet_name, json_packet_data)
-        // client.disconnect("reason") // force disconnect the client - may break other plugins
+        // player.write(packet_name, json_packet_data)
+        // player.disconnect("reason") // force disconnect the player - may break other plugins
         break;
       case "/stopserver":
-        client.sendMessage("Stopping server...");
+        player.sendMessage("Stopping server...");
         ShutdownAPI.shutdownServer();
         break;
     }
@@ -123,33 +123,33 @@ module.exports = {
     // This code executes when console executes a command
   },
 
-  onInternalServerError(server, client, error) {
+  onInternalServerError(server, player, error) {
     // This code executes when there is an server error
   },
 
-  onPlayerMove(server, client, location) {
+  onPlayerMove(server, player, location) {
     // This code executes when player moves
   },
 
-  onGamemodeChange(server, client, gamemode) {
+  onGamemodeChange(server, player, gamemode) {
     // This code executes when player changes his own gamemode
   },
 
-  onServerToClientChat(server, client, msg) {
-    // This code executes when server sends a chat message to client (useful for custom logging)
+  onServerToplayerChat(server, player, msg) {
+    // This code executes when server sends a chat message to player (useful for custom logging)
   },
 
-  onToast(client, server, title, msg) {
-    // This code executes when server sends toast to client
+  onToast(player, server, title, msg) {
+    // This code executes when server sends toast to player
   },
 
-  onTransfer(client, server, address, port) {
+  onTransfer(player, server, address, port) {
     // This code executes when player transfers to another server
-    // WARNING: Functions like client.sendMessage(), client.transfer() will not work anymore on that player
+    // WARNING: Functions like player.sendMessage(), player.transfer() will not work anymore on that player
   },
 
-  onFormResponse(server, client, packet) {
-    client.sendMessage("Response: " + JSON.stringify(packet).toString());
+  onFormResponse(server, player, packet) {
+    player.sendMessage("Response: " + JSON.stringify(packet).toString());
     // This code executes when:
     // a) Player clicks a button in a form
     // b) Player closes a form
