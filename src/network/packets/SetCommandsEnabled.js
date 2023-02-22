ca1743c0797d4926df7fb9e90495864edb879ccd
faaxz <126027982+faaxz@users.noreply.github.com>
2023-02-22 09:32:20 +0100

    Update SetCommandsEnabled.js

--- src/network/packets/SetCommandsEnabled.js
+++ src/network/packets/SetCommandsEnabled.js
@@ -14,30 +14,28 @@
 let enabled = true;
 
 class SetCommandsEnabled extends require("./Packet") {
-  /**
-   * @returns The name of the packet.
-   */
   name() {
     return "set_commands_enabled";
   }
 
   /**
-   * It returns if commands are enabled or not.
-   * @param {Boolean} enabled1 - Are commands enabled.
+   * Returns if commands are enabled or not.
+   * @param {Boolean} enabled1 - Are commands enabled or not
    */
   setEnabled(enabled1) {
     enabled = enabled1;
   }
 
   /**
-   * It returns if commands are enabled or not.
-   * @returns If the commands are enabled.
+   * Returns if commands are enabled or not
+   * @returns If the commands are enabled
    */
   getEnabled() {
     return enabled;
   }
 
   /**
+   * Sends the packet
    * @param {Object} client
    */
   send(client) {
