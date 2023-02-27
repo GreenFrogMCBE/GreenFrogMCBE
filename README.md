53d5dee2ec6b7a63378e77717496f373dd220a1f
andriycraft <kinashtesttest@gmail.com>
2023-02-27 12:00:18 +0100

    World: Added world settings file

--- README.md
+++ README.md
@@ -82,11 +82,12 @@ If you have one, feel free to open an issue (https://github.com/andriycraft/Gree
 2. package.json - (Do not edit if you don't know what are you doing) Config for some dependency related things
 3. src\lang\<server language>.json - A place, where all the translation string are stored.
 4. world\chunks.json - A save file for world
-5. world\custom_items.json - Allows for you to create custom items on your server
-6. src\network\packets\biomes.json - Allows for you to configure biomes
-7. src\network\packets\creativeContent.json - Allows for you to customize the creative menu. Useful to remove some items, and add custom
-7. src\network\packets\skinData.json - Allows to customize the skin data for the player list
-9. src\network\packets\entities.json - Allows to disable/enable entities
+5. world\world_settings.json - Here you can change the world name and other settings
+6. world\custom_items.json - Allows for you to create custom items on your server
+7. src\network\packets\biomes.json - Allows for you to configure biomes
+8. src\network\packets\creativeContent.json - Allows for you to customize the creative menu. Useful to remove some items, and add custom
+9. src\network\packets\skinData.json - Allows to customize the skin data for the player list
+10. src\network\packets\entities.json - Allows to disable/enable entities
 
 ## Credits
 
--- src/network/packets/handlers/ResourcePackClientResponse.js
+++ src/network/packets/handlers/ResourcePackClientResponse.js
@@ -126,19 +126,20 @@ class ResourcePackClientResponse extends Handler {
           startgame.setDifficulty(Difficulty.NORMAL);
           startgame.setSpawnPosition(0, 0, 0);
           startgame.setPlayerPermissionLevel(client.permlevel);
+          startgame.setWorldName(require("..\\..\\..\\..\\world\\world_settings.json").worldname)
           startgame.send(client);
 
           const biomedeflist = new BiomeDefinitionList();
-          biomedeflist.setValue(require("../res/biomes.json"));
+          biomedeflist.setValue(require("..\\res\\biomes.json"));
           biomedeflist.send(client);
 
           const availableentityids = new AvailableEntityIdentifiers();
-          availableentityids.setValue(require("../res/entities.json"));
+          availableentityids.setValue(require("..\\res\\entities.json"));
           availableentityids.send(client);
 
           const creativecontent = new CreativeContent();
           creativecontent.setItems(
-            require("../res/creativeContent.json").items
+            require("..\\res\\creativeContent.json").items
           );
           creativecontent.send(client);
 
@@ -242,7 +243,7 @@ class ResourcePackClientResponse extends Handler {
           // This packet is used to set custom items
           const itemcomponent = new ItemComponent()
           try {
-            itemcomponent.setItems(require("../../../../world/custom_items.json").items)
+            itemcomponent.setItems(require("..\\..\\..\\..\\world\\custom_items.json").items)
           } catch (e) {
             itemcomponent.setItems([])
           }
