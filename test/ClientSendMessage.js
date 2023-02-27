f9a9d96501c5eb09162993159992f916dd8df05c
anybananagame <ainaraspirmas@gmail.com>
2023-02-27 17:36:31 +0200

    Client Send Message test

---
+++ test/ClientSendMessage.js
@@ -0,0 +1,40 @@
+/**
+ * ░██████╗░██████╗░███████╗███████╗███╗░░██╗███████╗██████╗░░█████╗░░██████╗░
+ * ██╔════╝░██╔══██╗██╔════╝██╔════╝████╗░██║██╔════╝██╔══██╗██╔══██╗██╔════╝░
+ * ██║░░██╗░██████╔╝█████╗░░█████╗░░██╔██╗██║█████╗░░██████╔╝██║░░██║██║░░██╗░
+ * ██║░░╚██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║██╔══╝░░██╔══██╗██║░░██║██║░░╚██╗
+ * ╚██████╔╝██║░░██║███████╗███████╗██║░╚███║██║░░░░░██║░░██║╚█████╔╝╚██████╔╝
+ * ░╚═════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝╚═╝░░░░░╚═╝░░╚═╝░╚════╝░░╚═════╝░
+ *
+ *
+ * Copyright 2023 andriycraft
+ * Github: https://github.com/andriycraft/GreenFrogMCBE
+ */
+const bedrock = require("frog-protocol");
+
+module.exports = {
+  async test() {
+    await console.log("[client] joining...");
+    let Bot =
+      bedrock.createClient({
+        host: "127.0.0.1",
+        port: 19132,
+        username: "bot",
+        offline: true,
+        version: "1.19.63",
+      });
+
+
+    console.log("[client] joined");
+    Bot.on("spawn", () => {
+      Bot.queue("text", {
+        type: "chat",
+        needs_translation: false,
+        source_name: Bot.username,
+        xuid: "",
+        platform_chat_id: "",
+        message: `Hello!`,
+      });
+    });
+  },
+};
