6e007dfdd1f0969d7a40fd8526bc1d0631176f38
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-03-07 16:49:28 +0100

    API: Added major/minor server version

--- src/SoftwareInfo.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    majorServerVersion: "3.0",
-    minorServerVersion: "3.0",
-    apiVersion: "3.0"
-}
--- src/server/ServerInfo.js
+++ src/server/ServerInfo.js
@@ -1,21 +1,10 @@
-/**
- * ░██████╗░██████╗░███████╗███████╗███╗░░██╗███████╗██████╗░░█████╗░░██████╗░
- * ██╔════╝░██╔══██╗██╔════╝██╔════╝████╗░██║██╔════╝██╔══██╗██╔══██╗██╔════╝░
- * ██║░░██╗░██████╔╝█████╗░░█████╗░░██╔██╗██║█████╗░░██████╔╝██║░░██║██║░░██╗░
- * ██║░░╚██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║██╔══╝░░██╔══██╗██║░░██║██║░░╚██╗
- * ╚██████╔╝██║░░██║███████╗███████╗██║░╚███║██║░░░░░██║░░██║╚█████╔╝╚██████╔╝
- * ░╚═════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝╚═╝░░░░░╚═╝░░╚═╝░╚════╝░░╚═════╝░
- *
- *
- * Copyright 2023 andriycraft
- * Github: https://github.com/andriycraft/GreenFrogMCBE
- */
 const yaml = require("js-yaml");
 const fs = require("fs");
 
 module.exports = {
-	lang: require(`../lang/${yaml.load(fs.readFileSync("config.yml", "utf8")).lang}.json`),
+    majorServerVersion: "3.0",
+    minorServerVersion: "3.0",
+    apiVersion: "3.0",
+    lang: require(`../lang/${yaml.load(fs.readFileSync("config.yml", "utf8")).lang}.json`),
 	config: yaml.load(fs.readFileSync("config.yml", "utf8")),
-	majorserverversion: "3.0",
-	serverversion: "3.0",
-};
+}
