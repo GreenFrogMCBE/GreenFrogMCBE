45fe76d8132cb8a385661f25d5f5611c45d9b422
hvlxh <monsta22019@gmail.com>
2023-03-08 20:54:12 +0530

    Removed Command System [Your command system sucks like yours.]

--- src/plugin/events/PlayerCommandExecuteEvent.js
+++ src/plugin/events/PlayerCommandExecuteEvent.js
@@ -14,19 +14,7 @@
 const Event = require("./Event");
 const Logger = require("../../server/Logger");
 const { config, lang } = require("../../server/ServerInfo");
-const CommandPl = require("../../server/commands/CommandPl");
-const CommandMe = require("../../server/commands/CommandMe");
-const CommandOp = require("../../server/commands/CommandOp");
-const CommandManager = require("../../player/CommandManager");
-const CommandSay = require("../../server/commands/CommandSay");
-const CommandList = require("../../server/commands/CommandList");
-const CommandTime = require("../../server/commands/CommandTime");
-const CommandKick = require("../../server/commands/CommandKick");
-const CommandDeop = require("../../server/commands/CommandDeop");
-const CommandStop = require("../../server/commands/CommandStop");
-const CommandVersion = require("../../server/commands/CommandVersion");
 const FailedToHandleEvent = require("./exceptions/FailedToHandleEvent");
-const CommandGamemode = require("../../server/commands/CommandGamemode");
 
 const fs = require("fs");
 
@@ -59,71 +47,7 @@ class PlayerCommandExecuteEvent extends Event {
 	}
 
 	postExecute(client, message) {
-		if (!this.isCancelled() || config.commandsDisabled) {
-			Logger.log(lang.commands.executedCmd.replace("%player%", client.username).replace("%cmd%", message));
-
-			const cmdGamemode = new CommandGamemode();
-			const cmdManager = new CommandManager();
-			const cmdVer = new CommandVersion();
-			const cmdPl = new CommandPl();
-			const cmdStop = new CommandStop();
-			const cmdSay = new CommandSay();
-			const cmdOp = new CommandOp();
-			const cmdKick = new CommandKick();
-			const cmdTime = new CommandTime();
-			const cmdDeop = new CommandDeop();
-			const cmdList = new CommandList();
-			const cmdMe = new CommandMe();
-
-			let exists = false;
-			for (let i = 0; i < cmdManager.getCommands().length; i++) {
-				if (`${cmdManager.getCommands()[i].name.toLowerCase()}`.startsWith(message.replace("/", "").split(" ")[0].replace(" ", ""))) {
-					exists = true;
-					break;
-				}
-			}
-			if (!exists || message === "/") {
-				client.sendMessage(lang.errors.playerUnknownCommandOrNoPermission.replace("%commandname%", message));
-			} else {
-				const commands = {
-					ver: `/${lang.commands.ver.toLowerCase()}`,
-					version: `/${lang.commands.version.toLowerCase()}`,
-					pl: `/${lang.commands.pl.toLowerCase()}`,
-					plugins: `/${lang.commands.plugins.toLowerCase()}`,
-					stop: `/${lang.commands.stop.toLowerCase()}`,
-					say: `/${lang.commands.say.toLowerCase()}`,
-					op: `/${lang.commands.op.toLowerCase()}`,
-					kick: `/${lang.commands.kick.toLowerCase()}`,
-					time: `/${lang.commands.time.toLowerCase()}`,
-					deop: `/${lang.commands.deop.toLowerCase()}`,
-					list: `/${lang.commands.listc.toLowerCase()}`,
-					me: `/${lang.commands.me.toLowerCase()}`,
-					gamemode: `/gamemode`,
-				};
-
-				const commandsToExecute = {
-					[commands.ver]: cmdVer,
-					[commands.version]: cmdVer,
-					[commands.pl]: cmdPl,
-					[commands.plugins]: cmdPl,
-					[commands.stop]: cmdStop,
-					[commands.say]: cmdSay,
-					[commands.op]: cmdOp,
-					[commands.kick]: cmdKick,
-					[commands.time]: cmdTime,
-					[commands.deop]: cmdDeop,
-					[commands.list]: cmdList,
-					[commands.me]: cmdMe,
-					[commands.gamemode]: cmdGamemode,
-				};
-
-				const commandFound = Object.keys(commandsToExecute).find((command) => message.startsWith(command));
-
-				if (commandFound) {
-					commandsToExecute[commandFound].executePlayer(client, message);
-				}
-			}
-		}
+		// TODO: Adding Command Support
 	}
 }
 
--- src/plugin/events/ServerConsoleCommandExecutedEvent.js
+++ src/plugin/events/ServerConsoleCommandExecutedEvent.js
@@ -13,19 +13,6 @@
 /* eslint-disable no-case-declarations */
 /* eslint-disable no-unused-vars */
 const FailedToHandleEvent = require("./exceptions/FailedToHandleEvent");
-const Version = require("../../server/commands/CommandVersion");
-const { lang, config } = require("../../server/ServerInfo");
-const Stop = require("../../server/commands/CommandStop");
-const Kick = require("../../server/commands/CommandKick");
-const Help = require("../../server/commands/CommandHelp");
-const Time = require("../../server/commands/CommandTime");
-const List = require("../../server/commands/CommandList");
-const Deop = require("../../server/commands/CommandDeop");
-const Say = require("../../server/commands/CommandSay");
-const Op = require("../../server/commands/CommandOp");
-const PL = require("../../server/commands/CommandPl");
-const Me = require("../../server/commands/CommandMe");
-const Logger = require("../../server/Logger");
 const Event = require("./Event");
 
 const fs = require("fs");
@@ -59,107 +46,7 @@ class ServerConsoleCommandExecutedEvent extends Event {
 	}
 
 	postExecute(cmd) {
-		if (!this.isCancelled() || !config.disable) {
-			const commands = {
-				stop: new Stop(),
-				kick: new Kick(),
-				version: new Version(),
-				help: new Help(),
-				time: new Time(),
-				say: new Say(),
-				op: new Op(),
-				pl: new PL(),
-				deop: new Deop(),
-				list: new List(),
-				me: new Me(),
-			};
-
-			if (cmd.toLowerCase().startsWith(`${lang.commands.time.toLowerCase()} `)) {
-				commands.time.execute(cmd.split(" "));
-				return;
-			}
-
-			if (cmd.toLowerCase().startsWith(`${lang.commands.say.toLowerCase()} `)) {
-				const msg = cmd.split(" ").slice(1).join(" ");
-				commands.say.execute(msg);
-				return;
-			}
-
-			if (cmd.toLowerCase().startsWith(`${lang.commands.listc.toLowerCase()} `)) {
-				commands.list.execute();
-				return;
-			}
-
-			if (cmd.toLowerCase().startsWith(`${lang.commands.kick.toLowerCase()} `)) {
-				const dataParts = cmd.split(" ");
-				const target = dataParts[1];
-				const reason = dataParts.slice(2).join(" ");
-				commands.kick.execute([target, reason]);
-				return;
-			}
-
-			if (cmd.toLowerCase().startsWith(`${lang.commands.op.toLowerCase()} `)) {
-				commands.op.execute(cmd.split(" ")[1]);
-				return;
-			}
-
-			if (cmd.toLowerCase().startsWith(`${lang.commands.deop.toLowerCase()} `)) {
-				commands.deop.execute(cmd.split(" ")[1]);
-				return;
-			}
-
-			if (cmd.toLowerCase().startsWith(`${lang.commands.me.toLowerCase()} `)) {
-				commands.me.execute(cmd.split(" ").slice(1).join(" "));
-				return;
-			}
-
-			const command = cmd.toLowerCase().split(" ")[0];
-
-			switch (command) {
-				case "":
-					break;
-				case lang.commands.listc:
-					commands.list.execute();
-					break;
-				case lang.commands.stop.toLowerCase():
-					commands.stop.execute();
-					break;
-				case lang.commands.op.toLowerCase():
-					commands.op.execute(command.split(" ")[1]);
-					break;
-				case lang.commands.kick.toLowerCase():
-					const reason = command.split(" ").slice(2).join(" ");
-					commands.kick.execute(command.split(" ")[1], reason);
-					break;
-				case lang.commands.pl.toLowerCase():
-				case lang.commands.plugins.toLowerCase():
-					commands.pl.execute();
-					break;
-				case lang.commands.ver.toLowerCase():
-				case lang.commands.version.toLowerCase():
-					commands.version.execute();
-					break;
-				case lang.commands.me.toLowerCase():
-					commands.me.execute();
-					break;
-				case lang.commands.time.toLowerCase():
-					commands.time.execute();
-					break;
-				case lang.commands.say.toLowerCase():
-					commands.say.execute();
-					break;
-				case lang.commands.deop.toLowerCase():
-					commands.deop.execute();
-					break;
-				case "?":
-				case lang.commands.help.toLowerCase():
-					commands.help.execute();
-					break;
-				default:
-					Logger.log(lang.commands.unknownCommand);
-					break;
-			}
-		}
+		// TODO: Make the commands system better.
 	}
 }
 
