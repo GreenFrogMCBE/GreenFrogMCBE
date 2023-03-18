d557aac0485a15fd4b8b5cc2d01f5a4053e3a889
hvlxh <monsta22019@gmail.com>
2023-03-18 09:12:23 +0530

    Command: Time

---
+++ src/server/commands/CommandTime.js
@@ -0,0 +1,68 @@
+const Logger = require("../Logger");
+const { players } = require("../../player/PlayerInfo");
+const { lang, config } = require("../../server/ServerInfo");
+
+/**
+ * @type {import('../../base/Command').Command}
+ */	
+module.exports = {
+	runAsConsole(_server, args) {
+        let time = args[1];
+		switch (time) {
+			case "day":
+				time = 1000;
+				break;
+			case "night":
+				time = 17000;
+				break;
+			default:
+				time = parseInt(args[1]);
+				if (isNaN(time)) {
+					Logger.log(lang.commands.usageTime);
+					return;
+				}
+		}
+
+		for (const client of players) {
+			client.setTime(time);
+		}
+
+		Logger.log(lang.commands.timeUpdated);
+    },
+
+	run(_server, player, args) {
+        if (!config.consoleCommandTime) {
+			player.sendMessage("§c" + lang.errors.unknownCommand);
+			return;
+		}
+
+		let time = args[1];
+		switch (time) {
+			case "day":
+				time = 1000;
+				break;
+			case "night":
+				time = 17000;
+				break;
+			default:
+				time = parseInt(args[1]);
+				if (isNaN(time)) {
+					player.sendMessage("§c" + lang.commands.usageTime);
+					return;
+				}
+		}
+
+		for (const client of players) {
+			client.setTime(time);
+		}
+
+		player.sendMessage(lang.commands.timeUpdated);
+    },
+
+	data: {
+		name: "time",
+		description: "Time command.",
+		minArg: 1,
+		maxArg: 1,
+	},
+};
