df1d3ffd1f143eabbab5aed988ab1e3aedff1640
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-03-25 11:52:22 +0100

    bruh

--- src/events/PlayerKickEvent.js
+++ src/events/PlayerKickEvent.js
@@ -11,32 +11,16 @@
  * Github: https://github.com/andriycraft/GreenFrogMCBE
  */
 /* eslint-disable no-unused-vars */
-const UnsupportedOperationException = require("./exceptions/UnsupportedOperationException");
-const FailedToHandleEvent = require("./exceptions/FailedToHandleEvent");
 const Event = require("./Event");
-const fs = require("fs");
 
 class PlayerKickEvent extends Event {
 	constructor() {
 		super();
 		this.name = "PlayerKickEvent";
-		this.cancelled = false
 	}
 
-	cancel() {
-		throw new UnsupportedOperationException("Cancelling this event is not supported")
-	}
-
-	execute(server, client, msg) {
-		fs.readdir("./plugins", (err, plugins) => {
-			plugins.forEach((plugin) => {
-				try {
-					require(`${__dirname}/../../plugins/${plugin}`).PlayerKickEvent(server, client, msg, this);
-				} catch (e) {
-					FailedToHandleEvent.handleEventError(e, plugin, this.name);
-				}
-			});
-		});
+	execute() {
+		this._execute()
 	}
 }
 
