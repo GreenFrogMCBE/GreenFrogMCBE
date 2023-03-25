1086ebba408e01d26c0dabd2ed419ca9aa3d446a
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-03-25 11:55:12 +0100

    PlayerRequestChunkRadiusEvent

--- src/events/PlayerRequestChunkRadiusEvent.js
+++ src/events/PlayerRequestChunkRadiusEvent.js
@@ -1,40 +1,25 @@
-const fs = require('fs')
 const Event = require("./Event");
-const FailedToHandleEvent = require("./exceptions/FailedToHandleEvent");
-const worldSettings = require("../../world/world_settings.json");
 
 class PlayerRequestChunkRadiusEvent extends Event {
   constructor() {
     super();
     this.cancelled = false;
     this.name = "PlayerRequestChunkRadiusEvent";
+    this.worldSettings = null
+    this.server = null
+    this.player = null
+    this.radius = null
   }
 
   cancel() {
     this.cancelled = true;
   }
 
-  async execute(player, server, radius) {
-    await new Promise((resolve) => {
-      fs.readdir("./plugins", (err, plugins) => {
-        plugins.forEach((plugin) => {
-          try {
-            require(`${__dirname}/../../../plugins/${plugin}`).PlayerRequestChunkRadiusEvent(
-              player,
-              server,
-              radius,
-              this
-            );
-          } catch (e) {
-            FailedToHandleEvent.handleEventError(e, plugin, this.name);
-          }
-        });
-        resolve();
-      });
-    });
+  async execute() {
+    this._execute()
 
     if (!this.cancelled) {
-      player.setChunkRadius(worldSettings.chunkLoadRadius);
+      this.player.setChunkRadius(this.worldSettings.chunkLoadRadius);
     }
   }
 }
