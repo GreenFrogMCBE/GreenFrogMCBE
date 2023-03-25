da013dc97f9d779032e758daae8e1fd816fb4fcc
bufferfishq <124588671+bufferfishq@users.noreply.github.com>
2023-03-25 17:47:38 +0100

    Update TestConfigs.js

--- test/TestConfigs.js
+++ test/TestConfigs.js
@@ -11,18 +11,14 @@
  * Github: https://github.com/andriycraft/GreenFrogMCBE
  */
 
-/**
- * @type {import('../types/base/Plugin').default}
- */
-
 module.exports = {
 	test() {
 		const files = [
 			"/../src/lang/en_US.json",
-			//"/../src/lang/lt_LT.json",
-			//"/../src/lang/uk_UA.json",
-			//"/../src/lang/vi_VN.json",
-			//"/../src/lang/fr_FR.json",
+			"/../src/lang/lt_LT.json",
+			"/../src/lang/uk_UA.json",
+			"/../src/lang/vi_VN.json",
+			"/../src/lang/fr_FR.json",
 			"/../src/network/packets/res/biomes.json",
 			"/../src/network/packets/res/creativeContent.json",
 			"/../src/network/packets/res/skinData.json",
