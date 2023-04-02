c8a2ee7787015d221d4075ea7b2b78b3f8692b74
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-04-02 16:36:06 +0200

    better error handling

--- src/events/ServerConsoleCommandExecutedEvent.js
+++ src/events/ServerConsoleCommandExecutedEvent.js
@@ -33,38 +33,42 @@ class ServerConsoleCommandExecutedEvent extends Event {
 		await this._execute(this);
 
 		if (!this.cancelled) {
-			const cmds = await readdir("./src/commands");
+			try {
+				const cmds = await readdir("./src/commands");
 
-			let exists = false;
-			const name = this.command.split(" ")[0];
-			const args = this.command.split(" ").slice(1);
+				let exists = false;
+				const name = this.command.split(" ")[0];
+				const args = this.command.split(" ").slice(1);
 
-			for (const camd of cmds) {
-				/**
-				 * @type {import('../../base/Command').Command}
-				 */
-				const command = require(`../../server/commands/${camd}`);
+				for (const camd of cmds) {
+					/**
+					 * @type {import('../../base/Command').Command}
+					 */
+					const command = require(`../commands/${camd}`);
 
-				if (command.data.name === name || command.data.aliases && command.data.aliases.includes(name)) {
-					if (command.data.minArg && command.data.minArg > args.length) {
-						Logger.info(lang.commands.minArg.replace("%m%", command.data.minArg).replace("%r%", args.length));
+					if (command.data.name === name || command.data.aliases && command.data.aliases.includes(name)) {
+						if (command.data.minArg && command.data.minArg > args.length) {
+							Logger.info(lang.commands.minArg.replace("%m%", command.data.minArg).replace("%r%", args.length));
+							exists = true;
+							return;
+						}
+
+						if (command.data.maxArg && command.data.maxArg < args.length) {
+							Logger.info(lang.commands.maxArg.replace("%m%", command.data.maxArg).replace("%r%", args.length));
+							exists = true;
+							return;
+						}
+
+						command.runAsConsole(this.server, args);
 						exists = true;
-						return;
 					}
-
-					if (command.data.maxArg && command.data.maxArg < args.length) {
-						Logger.info(lang.commands.maxArg.replace("%m%", command.data.maxArg).replace("%r%", args.length));
-						exists = true;
-						return;
-					}
-
-					command.runAsConsole(this.server, args);
-					exists = true;
 				}
-			}
 
-			if (!exists && this.command) {
-				Logger.info(lang.errors.unknownCommandOrNoPermission.replace('%commandname%', this.command));
+				if (!exists && this.command) {
+					Logger.info(lang.errors.unknownCommandOrNoPermission.replace('%commandname%', this.command));
+				}
+			} catch (e) {
+				Logger.error("Failed to execute command! " + e.stack)
 			}
 		}
 	}
