dde9ef4087b700ff86912e057b6650d29788e30a
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-04-10 20:14:50 +0200

    lmao x2

--- src/server/ConsoleCommandSender.js
+++ src/server/ConsoleCommandSender.js
@@ -1,29 +1,19 @@
-/**
- * ░██████╗░██████╗░███████╗███████╗███╗░░██╗███████╗██████╗░░█████╗░░██████╗░
- * ██╔════╝░██╔══██╗██╔════╝██╔════╝████╗░██║██╔════╝██╔══██╗██╔══██╗██╔════╝░
- * ██║░░██╗░██████╔╝█████╗░░█████╗░░██╔██╗██║█████╗░░██████╔╝██║░░██║██║░░██╗░
- * ██║░░╚██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║██╔══╝░░██╔══██╗██║░░██║██║░░╚██╗
- * ╚██████╔╝██║░░██║███████╗███████╗██║░╚███║██║░░░░░██║░░██║╚█████╔╝╚██████╔╝
- * ░╚═════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝╚═╝░░░░░╚═╝░░╚═╝░╚════╝░░╚═════╝░
- *
- *
- * Copyright 2023 andriycraft
- * Github: https://github.com/andriycraft/GreenFrogMCBE
- */
-/* eslint-disable no-case-declarations */
 const rl = require("readline");
 const ConsoleCommandExecutedEvent = require("../events/ServerConsoleCommandExecutedEvent");
 const Frog = require("../Frog");
+const { readdir } = require("fs/promises");
+const Logger = require("../Logger");
+const lang = require("../lang");
 
 let isclosed = false;
 
 module.exports = {
-	closed: isclosed,
-
+	get closed() {
+		return isclosed;
+	},
 	close() {
 		isclosed = true;
 	},
-
 	async start() {
 		const r = rl.createInterface({
 			input: process.stdin,
@@ -33,14 +23,93 @@ module.exports = {
 		r.setPrompt("");
 		r.prompt(true);
 
-		r.on("line", (command) => {
-			Frog.eventEmitter.on()
+		for await (const command of r) {
+			let shouldProcessCommand = false;
+
+			Frog.eventEmitter.on("serverCommandProcessEvent", (event) => {
+				if (event.command === command) {
+					event.cancel();
+					shouldProcessCommand = true;
+				}
+			});
+
+			if (shouldProcessCommand) {
+				try {
+					const cmds = await readdir("./src/commands");
+					const name = command.split(" ")[0];
+					const args = command.split(" ").slice(1);
+
+					if (!name.replace(" ", "")) return;
+
+					let exists = false;
+
+					for (const camd of cmds) {
+						if (camd.endsWith(".js")) {
+							const command = require(`../commands/${camd}`);
+
+							if (
+								command.data.name === name ||
+								(command.data.aliases && command.data.aliases.includes(name))
+							) {
+								if (
+									command.data.minArgs !== undefined &&
+									command.data.minArgs > args.length
+								) {
+									Logger.info(
+										lang.commands.minArg
+											.replace("%m%", command.data.minArgs)
+											.replace("%r%", args.length)
+									);
+									return;
+								}
+
+								if (
+									command.data.maxArgs !== undefined &&
+									command.data.maxArgs < args.length
+								) {
+									Logger.info(
+										lang.commands.maxArg
+											.replace("%m%", command.data.maxArgs)
+											.replace("%r%", args.length)
+									);
+									return;
+								}
+
+								command.execute(this.server, {
+										sendMessage: (message) => {
+											Logger.info(message);
+										},
+										op: true,
+										username: "Server",
+										ip: "127.0.0.1",
+										isConsole: true,
+									},
+									args
+								);
+
+								exists = true;
+							}
+						}
+					}
+
+					if (!exists)
+						Logger.info(
+							lang.errors.unknownCommandOrNoPermission.replace(
+								"%commandname%",
+								name
+							)
+						);
+				} catch (e) {
+					Logger.error("Failed to execute command! " + e.stack);
+				}
+			}
+
 			const commandExecutedEvent = new ConsoleCommandExecutedEvent();
 			commandExecutedEvent.server = require("../Server");
 			commandExecutedEvent.command = command;
 			commandExecutedEvent.execute();
 
 			if (!isclosed) r.prompt(true);
-		});
+		}
 	},
 };
