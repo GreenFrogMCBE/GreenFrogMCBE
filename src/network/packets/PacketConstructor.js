77c5c4472212e166781b5187822f8bdf82684185
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-04-13 01:00:45 +0200

    PacketConstructor: Added jsdoc

--- src/network/packets/PacketConstructor.js
+++ src/network/packets/PacketConstructor.js
@@ -15,20 +15,39 @@ const UnsupportedOperationException = require("../../events/exceptions/Unsupport
 class PacketConstructor {
 	constructor() {}
 
+	/**
+	 * Returns the packet name
+	 * 
+	 * @returns {String} 
+	 */
 	getPacketName() {
 		return "Unknown";
 	}
 
+	/**
+	 * Returns if the packet is critical
+	 * 
+	 * @returns {Boolean}
+	 */
 	isCriticalPacket() {
 		return false;
 	}
 
+	/**
+	 * Validates the packet
+	 */
 	validatePacket() {}
 
+	/**
+	 * Writes the packet
+	 */
 	writePacket() {
 		throw new UnsupportedOperationException("Can't write a client-side packet");
 	}
 
+	/**
+	 * Reads the packet
+	 */
 	readPacket() {
 		throw new UnsupportedOperationException("Can't read a server-side packet");
 	}
