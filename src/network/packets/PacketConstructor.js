ded9d47ec4c8222ae06becccf45b1f5cd61cb39d
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-04-13 01:03:25 +0200

    [nochangelog] fix

--- src/network/packets/PacketConstructor.js
+++ src/network/packets/PacketConstructor.js
@@ -10,7 +10,7 @@
  * Copyright 2023 andriycraft
  * Github: https://github.com/andriycraft/GreenFrogMCBE
  */
-const UnsupportedOperationException = require("../../events/exceptions/UnsupportedOperationException");
+const UnsupportedOperationException = require("../../utils/exceptions/UnsupportedOperationException");
 
 class PacketConstructor {
 	constructor() {}
