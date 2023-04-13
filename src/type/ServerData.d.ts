6e3bdd7a568526d09b9a144de9ace4ba21d094a5
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-04-13 17:49:14 +0200

    [nochangelog] fix serverdata

--- src/type/ServerData.d.ts
+++ src/type/ServerData.d.ts
@@ -1,13 +1,7 @@
-interface ServerData {
+export interface ServerData {
     minorServerVersion: string;
     majorServerVersion: string;
     apiServerVersion: string;
 }
 
-function getServerData(): ServerData {
-    return {
-        minorServerVersion: "3.1 (API rewrite)",
-        majorServerVersion: "3.0",
-        apiServerVersion: "3.0"
-    };
-}
+declare function getServerData(): ServerData;
