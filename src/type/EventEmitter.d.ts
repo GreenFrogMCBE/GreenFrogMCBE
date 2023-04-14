1ecf9c412fe0d40d13a9b94d08c98b86d224fc02
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-04-14 10:57:13 +0200

    added typings

--- src/type/EventEmitter.d.ts
+++ src/type/EventEmitter.d.ts
@@ -22,6 +22,8 @@ declare function on(eventName: "serverGamemodeChange", listener: (...args: any[]
 declare function on(eventName: "serverCommandProcess", listener: (...args: any[]) => void): void;
 declare function on(eventName: "serverCommandProcessError", listener: (...args: any[]) => void): void;
 declare function on(eventName: "serverLogMessage", listener: (...args: any[]) => void): void;
+declare function on(eventName: "serverSetEntityData", listener: (...args: any[]) => void): void;
+declare function on(eventName: "serverSetDifficulty", listener: (...args: any[]) => void): void;
 declare function on(eventName: "playerTransferEvent", listener: (...args: any[]) => void): void;
 declare function on(eventName: "playerKickEvent", listener: (...args: any[]) => void): void;
 
