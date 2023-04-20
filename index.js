3a48d888310557ee85f0db15bb764f46967498e1
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-04-20 11:43:55 +0200

    Server: Updated to use `error` instead of `e.stack`

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-/**
- * ░██████╗░██████╗░███████╗███████╗███╗░░██╗███████╗██████╗░░█████╗░░██████╗░
- * ██╔════╝░██╔══██╗██╔════╝██╔════╝████╗░██║██╔════╝██╔══██╗██╔══██╗██╔════╝░
- * ██║░░██╗░██████╔╝█████╗░░█████╗░░██╔██╗██║█████╗░░██████╔╝██║░░██║██║░░██╗░
- * ██║░░╚██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║██╔══╝░░██╔══██╗██║░░██║██║░░╚██╗
- * ╚██████╔╝██║░░██║███████╗███████╗██║░╚███║██║░░░░░██║░░██║╚█████╔╝╚██████╔╝
- * ░╚═════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝╚═╝░░░░░╚═╝░░╚═╝░╚════╝░░╚═════╝░
- *
- *
- * Copyright 2023 andriycraft
- * Github: https://github.com/andriycraft/GreenFrogMCBE
- */
 const fs = require("fs");
 const center = require("center-align");
-
 const Colors = require('./src/api/colors/Colors');
 const { convertConsoleColor } = require('./src/utils/ConsoleColorConvertor');
 
@@ -22,25 +9,24 @@ console.info(convertConsoleColor(center(`${Colors.GREEN}
 ██   ███ ██████  █████   █████   ██ ██  ██ █████   ██████  ██    ██ ██   ███ 
 ██    ██ ██   ██ ██      ██      ██  ██ ██ ██      ██   ██ ██    ██ ██    ██ 
 ██████  ██   ██ ███████ ███████ ██   ████ ██      ██   ██  ██████   ██████  
-${Colors.RESET}`, process.stdout.columns)))
+${Colors.RESET}`, process.stdout.columns)));
 
-const crashFileName = './crash-reports/server-crash-' + Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + '.txt'
+const crashFileName = `./crash-reports/server-crash-${Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)}.txt`;
 
 async function createConfigFilesAndDebug() {
 	if (!fs.existsSync("config.yml")) {
-		const config = fs.readFileSync('./src/internalResources/defaultConfig.yml')
-
-		fs.writeFileSync('config.yml', config, () => { })
-
-		return
+		const config = fs.readFileSync('./src/internalResources/defaultConfig.yml');
+		fs.writeFileSync('config.yml', config, () => { });
 	}
 
-	if (require('./src/Frog').isDebug) process.env.DEBUG = 'minecraft-protocol'
+	if (require('./src/Frog').isDebug) {
+		process.env.DEBUG = 'minecraft-protocol';
+	}
 }
 
 async function start() {
 	try {
-		await createConfigFilesAndDebug()
+		await createConfigFilesAndDebug();
 
 		const Server = require("./src/Server.js");
 		Server.start();
@@ -48,22 +34,21 @@ async function start() {
 		process.once("SIGINT", async () => {
 			require("./src/Frog").shutdownServer();
 		});
-	} catch (e) {
-		console.clear()
+	} catch (error) {
+		console.clear();
 
 		console.error(convertConsoleColor(`${Colors.RED}Failed to start server
-	${e.stack}
+${error}
 	
-	Make sure that you have the required libraries. Run "npm i" to install them
-	If you are sure that this is a bug please report it here: https://github.com/andriycraft/GreenFrogMCBE
-	${Colors.RESET}
-	`));
+Make sure that you have the required libraries. Run "npm i" to install them
+If you are sure that this is a bug please report it here: https://github.com/andriycraft/GreenFrogMCBE
+${Colors.RESET}`));
 
-		fs.mkdir('crash-reports', { recursive: true }, () => { })
-		fs.writeFileSync(crashFileName, `Error: ${e.stack}`, () => { })
+		fs.mkdir('crash-reports', { recursive: true }, () => { });
+		fs.writeFileSync(crashFileName, `Error: ${error.stack}`, () => { });
 
 		process.exit(-1);
 	}
 }
 
-start()
+start();
