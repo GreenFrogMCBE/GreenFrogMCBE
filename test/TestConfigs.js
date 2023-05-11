bac5e321bcc148d8d2e8874f9fc1932d45eba065
@kinash <92477814+andriycraft@users.noreply.github.com>
2023-05-11 09:33:15 +0200

    Update TestConfigs.js

--- test/TestConfigs.js
+++ test/TestConfigs.js
@@ -13,7 +13,19 @@
 
 module.exports = {
 	test() {
-		const files = ["/../src/lang/en_US.json", "/../src/internalResources/biomes.json", "/../src/internalResources/creativeContent.json", "/../src/internalResources/skinData.json", "/../src/internalResources/entities.json", "/../world/world_settings.json", "/../world/custom_items.json", "/../world/gamerules.json", "/../package.json", "/../package-lock.json"];
+		// prettier-ignore
+		const files = [
+			"/../src/lang/en_US.json",
+			"/../src/internalResources/biomes.json", 
+			"/../src/internalResources/creativeContent.json", 
+			"/../src/internalResources/skinData.json", 
+			"/../src/internalResources/entities.json", 
+			"/../world/world_settings.json", 
+			"/../world/custom_items.json", 
+			"/../world/gamerules.json", 
+			"/../package.json",
+			"/../package-lock.json"
+		];
 
 		for (const file of files) {
 			console.info(`[config files] Parsing: ${file}`);
