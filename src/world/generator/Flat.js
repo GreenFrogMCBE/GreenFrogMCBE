c8a2ac677d234031621685b20a53b174f3d293f1
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-05-22 10:06:54 +0200

    Flat: Code clean up

--- src/world/generator/Flat.js
+++ src/world/generator/Flat.js
@@ -13,48 +13,19 @@ class Flat extends Generator {
 
 	getChunkData() {
 		chunkData = Buffer.alloc(16 * 16 * 256);
-
 		for (let y = 0; y < 256; y++) {
 			for (let x = 0; x < 16; x++) {
 				for (let z = 0; z < 16; z++) {
 					const index = y * 16 * 16 + z * 16 + x;
 
-					if (y > 13 && x > 14) {
-						chunkData[index] = 2;
-					} else if (y > 12 && x > 14) {
-						chunkData[index] = 2;
-					} else if (y > 11 && x > 14) {
-						chunkData[index] = 2;
-					} else if (y > 10 && x > 14) {
-						chunkData[index] = 2;
-					} else if (y > 9 && x > 14) {
-						chunkData[index] = 2;
-					} else if (y > 8 && x > 14) {
-						chunkData[index] = 2;
-					} else if (y > 7 && x > 14) {
-						chunkData[index] = 2;
-					} else if (y > 6 && x > 14) {
-						chunkData[index] = 2;
-					} else if (y > 5 && x > 14) {
-						chunkData[index] = 2;
-					} else if (y > 4 && x > 14) {
-						chunkData[index] = 2;
-					} else if (y > 3 && x > 14) {
-						chunkData[index] = 2;
-					} else if (y > 2 && x > 14) {
-						chunkData[index] = 2;
-					} else if (y > 1 && x > 14) {
-						chunkData[index] = 2;
-					} else if (y > 0 && x > 14) {
-						chunkData[index] = 2;
-					} else if (y > -1 && x > 14) {
+					if (x > 14 && y > -1 && y < 16) {
 						chunkData[index] = 2;
 					} else if (y < 18 && x < 11) {
-						chunkData[index] = 0
+						chunkData[index] = 0;
 					} else if (y < 17 && x < 12) {
-						chunkData[index] = 7
+						chunkData[index] = 7;
 					} else {
-						chunkData[index] = 3; // Dirt blocks for other layers
+						chunkData[index] = 3;
 					}
 				}
 			}
