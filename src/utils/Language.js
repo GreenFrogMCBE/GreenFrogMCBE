e01777f21ca7b5fa48767fbf135df5119c21fe04
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-06-02 12:03:14 +0200

    revert this

--- src/utils/Language.js
+++ src/utils/Language.js
@@ -22,41 +22,43 @@ const LanguageException = require("./exceptions/LanguageException");
  * @typedef {Object.<string, string>} LanguageContent
  */
 
-module.exports = {
+/**
+ * Returns the content of a language file.
+ *
+ *
+ * @param {string} lang - The language code.
+ * @returns {LanguageContent} The content of the language file.
+ * @throws {LanguageException} If the language file is not found or is not valid JSON.
+ */
+function getLanguage(lang) {
+	const langPath = path.resolve(__dirname, "../lang");
+	const langFile = path.join(langPath, `${lang}.json`);
 
-	/**
-	 * Returns the content of a language file.
-	 *
-	 *
-	 * @param {string} lang - The language code.
-	 * @returns {LanguageContent} The content of the language file.
-	 * @throws {LanguageException} If the language file is not found or is not valid JSON.
-	 */
-	getLanguage(lang) {
-		const langPath = path.resolve(__dirname, "../lang");
-		const langFile = path.join(langPath, `${lang}.json`);
-
-		if (!existsSync(langFile)) {
-			throw new LanguageException("Failed to find that language");
-		}
-
-		const langContent = require(langFile);
-
-		if (typeof langContent !== "object") {
-			throw new LanguageException("Language file is not valid JSON");
-		}
-
-		return langContent;
-	},
-
-	/**
-	 * Returns a specific key from the current language file.
-	 *
-	 * @param {string} key - The key to retrieve.
-	 * @returns {string} The value associated with the key.
-	 */
-	getKey(key) {
-		const langConfig = require("../Frog").serverConfigurationFiles.config.chat.lang;
-		return this.getLanguage(langConfig)[key];
+	if (!existsSync(langFile)) {
+		throw new LanguageException("Failed to find that language");
 	}
+
+	const langContent = require(langFile);
+
+	if (typeof langContent !== "object") {
+		throw new LanguageException("Language file is not valid JSON");
+	}
+
+	return langContent;
 }
+
+/**
+ * Returns a specific key from the current language file.
+ *
+ * @param {string} key - The key to retrieve.
+ * @returns {string} The value associated with the key.
+ */
+function getKey(key) {
+	const langConfig = require("../Frog").serverConfigurationFiles.config.chat.lang;
+	return getLanguage(langConfig)[key];
+}
+
+module.exports = {
+	getLanguage,
+	getKey,
+};
