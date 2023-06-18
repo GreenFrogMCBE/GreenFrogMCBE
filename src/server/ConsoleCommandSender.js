81cd212b7d6f8b2bf25eea33d899bc3384451eae
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-06-18 16:58:46 +0200

    ConsoleCommandSender: Export `executeConsoleCommand()` so plugins can call it

--- src/server/ConsoleCommandSender.js
+++ src/server/ConsoleCommandSender.js
@@ -50,84 +50,6 @@ async function setupConsoleReader() {
 	readLineInterface.prompt(true);
 }
 
-/**
- * Executes a command that the user typed in the console.
- *
- * @param {string} executedCommand - The command to execute.
- * @throws {CommandHandlingException} Throws an error if the command cannot be executed.
- */
-async function executeConsoleCommand(executedCommand) {
-	let shouldExecCommand = true;
-
-	const args = executedCommand.split(" ").slice(1);
-
-	require("../Frog").eventEmitter.emit("serverExecutedCommand", {
-		server: require("../Frog").server,
-		args,
-		command: executedCommand,
-		cancel: () => {
-			shouldExecCommand = false;
-		},
-	});
-
-	if (isClosed || !shouldExecCommand || !executedCommand.replace(" ", "")) return;
-
-	try {
-		let commandFound = false;
-
-		for (const command of Commands.commandList) {
-			if (command.data.name === executedCommand.split(" ")[0] || (command.data.aliases && command.data.aliases.includes(executedCommand.split(" ")[0]))) {
-				if (command.data.minArgs !== undefined && command.data.minArgs > args.length) {
-					Logger.info(Language.getKey("commands.errors.syntaxError.minArg").replace("%s%", command.data.minArgs).replace("%d%", args.length));
-					return;
-				}
-
-				if (command.data.maxArgs !== undefined && command.data.maxArgs < args.length) {
-					Logger.info(Language.getKey("commands.errors.syntaxError.maxArg").replace("%s%", command.data.maxArgs).replace("%d%", args.length));
-					return;
-				}
-
-				command.execute(
-					Frog,
-					{
-						sendMessage: (message) => {
-							Logger.info(message);
-						},
-						setGamemode: () => {},
-						op: true,
-						username: "Server",
-						ip: "127.0.0.1",
-						isConsole: true,
-					},
-					args
-				);
-
-				commandFound = true;
-				break; // Exit loop once command has been found and executed
-			}
-		}
-
-		if (!commandFound) {
-			CommandVerifier.throwError(
-				{
-					sendMessage: (msg) => {
-						Logger.info(msg);
-					},
-				},
-				executedCommand.split(" ")[0]
-			);
-		}
-	} catch (error) {
-		require("../Frog").eventEmitter.emit("serverCommandProcessError", {
-			server: require("../Frog").server,
-			command: executedCommand,
-			error,
-		});
-
-		Logger.error(Language.getKey("commands.internalError").replace("%s%", error.stack));
-	}
-}
-
 module.exports = {
 	/**
 	 * Closes the console.
@@ -157,6 +79,84 @@ module.exports = {
 	readLineInterface,
 
 	/**
+	 * Executes a command that the user typed in the console.
+ 	 *
+ 	 * @param {string} executedCommand - The command to execute.
+ 	 * @throws {CommandHandlingException} Throws an error if the command cannot be executed.
+	 */
+	executeConsoleCommand(executedCommand) {
+		let shouldExecCommand = true;
+
+		const args = executedCommand.split(" ").slice(1);
+
+		require("../Frog").eventEmitter.emit("serverExecutedCommand", {
+			server: require("../Frog").server,
+			args,
+			command: executedCommand,
+			cancel: () => {
+				shouldExecCommand = false;
+			},
+		});
+
+		if (isClosed || !shouldExecCommand || !executedCommand.replace(" ", "")) return;
+
+		try {
+			let commandFound = false;
+
+			for (const command of Commands.commandList) {
+				if (command.data.name === executedCommand.split(" ")[0] || (command.data.aliases && command.data.aliases.includes(executedCommand.split(" ")[0]))) {
+					if (command.data.minArgs !== undefined && command.data.minArgs > args.length) {
+						Logger.info(Language.getKey("commands.errors.syntaxError.minArg").replace("%s%", command.data.minArgs).replace("%d%", args.length));
+						return;
+					}
+
+					if (command.data.maxArgs !== undefined && command.data.maxArgs < args.length) {
+						Logger.info(Language.getKey("commands.errors.syntaxError.maxArg").replace("%s%", command.data.maxArgs).replace("%d%", args.length));
+						return;
+					}
+
+					command.execute(
+						Frog,
+						{
+							sendMessage: (message) => {
+								Logger.info(message);
+							},
+							setGamemode: () => { },
+							op: true,
+							username: "Server",
+							ip: "127.0.0.1",
+							isConsole: true,
+						},
+						args
+					);
+
+					commandFound = true;
+					break; // Exit loop once command has been found and executed
+				}
+			}
+
+			if (!commandFound) {
+				CommandVerifier.throwError(
+					{
+						sendMessage: (msg) => {
+							Logger.info(msg);
+						},
+					},
+					executedCommand.split(" ")[0]
+				);
+			}
+		} catch (error) {
+			require("../Frog").eventEmitter.emit("serverCommandProcessError", {
+				server: require("../Frog").server,
+				command: executedCommand,
+				error,
+			});
+
+			Logger.error(Language.getKey("commands.internalError").replace("%s%", error.stack));
+		}
+	},
+
+	/**
 	 * Starts the console.
 	 */
 	async start() {
@@ -176,7 +176,7 @@ module.exports = {
 			});
 
 			if (shouldProcessCommand) {
-				executeConsoleCommand(command);
+				this.executeConsoleCommand(command);
 
 				if (!isClosed) readLineInterface.prompt(true);
 			}
