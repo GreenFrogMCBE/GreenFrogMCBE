791cb62a35d0c3af8e9743913b50324fc08513b8
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-06-23 18:31:47 +0200

    2 more

---
+++ src/block/runtimeIds/Glass.js
@@ -0,0 +1,32 @@
+927668178
+/**
+ * ░██████╗░██████╗░███████╗███████╗███╗░░██╗███████╗██████╗░░█████╗░░██████╗░
+ * ██╔════╝░██╔══██╗██╔════╝██╔════╝████╗░██║██╔════╝██╔══██╗██╔══██╗██╔════╝░
+ * ██║░░██╗░██████╔╝█████╗░░█████╗░░██╔██╗██║█████╗░░██████╔╝██║░░██║██║░░██╗░
+ * ██║░░╚██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║██╔══╝░░██╔══██╗██║░░██║██║░░╚██╗
+ * ╚██████╔╝██║░░██║███████╗███████╗██║░╚███║██║░░░░░██║░░██║╚█████╔╝╚██████╔╝
+ * ░╚═════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝╚═╝░░░░░╚═╝░░╚═╝░╚════╝░░╚═════╝░
+ *
+ * The content of this file is licensed using the CC-BY-4.0 license
+ * which requires you to agree to its terms if you wish to use or make any changes to it.
+ *
+ * @license CC-BY-4.0
+ * @link Github - https://github.com/andriycraft/GreenFrogMCBE
+ * @link Discord - https://discord.gg/UFqrnAbqjP
+ */
+const Block = require("./Block");
+
+/**
+ * @type {import('../../type/RuntimeBlock')}
+ */
+class Glass extends Block {
+	getRuntimeId() {
+		return 927668178;
+	}
+
+	getName() {
+		return "glass";
+	}
+}
+
+module.exports = Glass;
