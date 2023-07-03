b4e002256636579df9543c63268371f4f4e762f1
erixweb <erikmorenodev@gmail.com>
2023-07-03 15:24:38 +0200

    Add /tell,w,whisper,msg command

---
+++ src/commands/CommandTell.js
@@ -0,0 +1,42 @@
+/**
+ * ░██████╗░██████╗░███████╗███████╗███╗░░██╗███████╗██████╗░░█████╗░░██████╗░
+ * ██╔════╝░██╔══██╗██╔════╝██╔════╝████╗░██║██╔════╝██╔══██╗██╔══██╗██╔════╝░
+ * ██║░░██╗░██████╔╝█████╗░░█████╗░░██╔██╗██║█████╗░░██████╔╝██║░░██║██║░░██╗░
+ * ██║░░╚██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║██╔══╝░░██╔══██╗██║░░██║██║░░╚██╗
+ * ╚██████╔╝██║░░██║███████╗███████╗██║░╚███║██║░░░░░██║░░██║╚█████╔╝╚██████╔╝
+ * ░╚═════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝╚═╝░░░░░╚═╝░░╚═╝░╚════╝░░╚═════╝░
+ *
+ * The content of this file is licensed using the CC-BY-4.0 license
+ * which requires you to agree to its terms if you wish to use or make any changes to it.
+ *
+ * @license CC-BY-4.0
+ * @link Github - https://github.com/andriycraft/GreenFrogMCBE
+ * @link Discord - https://discord.gg/UFqrnAbqjP
+ */
+
+// Has to be translated
+
+
+module.exports = {
+	data: {
+		name: "tell",
+		description: "Send private messages",
+		aliases: ["w", "whisper", "msg"],
+		minArgs: 2,
+		maxArgs: 2,
+	},
+
+	execute(_server, player, args) {
+        const { get: getPlayerInfo } = require("../api/player/PlayerInfo")
+
+        const subject = args[0]
+        const message = args[1]
+
+        try {
+            getPlayerInfo(subject).sendMessage(`§7§oFrom ${player.username}: ${message}`)
+            player.sendMessage(`§7§oTo ${subject}: ${message}`)
+        } catch (e) {
+            player.sendMessage(`§cPlayer with the name of ${subject} isn't online.`)
+        }
+	},
+};
