d5fe7530a4fb97f06ca54d1bb7d78e446cc949bb
andriycraft <92477814+andriycraft@users.noreply.github.com>
2023-07-12 00:03:27 +0200

    rat

--- src/player/PlayerInit.js
+++ src/player/PlayerInit.js
@@ -10,7 +10,7 @@
  * which requires you to agree to its terms if you wish to use or make any changes to it.
  *
  * @license CC-BY-4.0
- * @link Github - https://github.com/andriycraft/GreenFrogMCBE
+ * @link Github - https://github.com/aboxofrats/GreenFrogMCBE
  * @link Discord - https://discord.gg/UFqrnAbqjP
  */
 const fs = require('fs')
