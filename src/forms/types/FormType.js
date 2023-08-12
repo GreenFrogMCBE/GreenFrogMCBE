8da0bec8ad8c43c7ecfdfdebba1e6fa688df0321
kotinash <92477814+kotinash@users.noreply.github.com>
2023-08-12 14:49:12 +0200

    Update FormType.js

--- src/forms/types/FormType.js
+++ src/forms/types/FormType.js
@@ -13,12 +13,8 @@
  * @link Github - https://github.com/GreenFrogMCBE/GreenFrogMCBE
  * @link Discord - https://discord.gg/UFqrnAbqjP
  */
-
 module.exports = /** @type {const} */ ({
-	/* A form type, that has support only buttons and text (PLEASE SEE THE EXAMPLE PLUGIN BECAUSE MOST FUNCTIONS WILL NOT WORK FOR THIS TYPE OF FORM: https://github.com/greenfrogmc/ExamplePlugin/blob/29d2675aefccb1c4efaf375ac1aca9895d4913b1/exampleplugin.js#L129) */
 	FORM: "form",
-	/* A form type, that supports every possible field in Minecraft (DOCS: https://github.com/greenfrogmc/ExamplePlugin/blob/29d2675aefccb1c4efaf375ac1aca9895d4913b1/exampleplugin.js#L129) */
 	CUSTOM_FORM: "custom_form",
-	/* A form type, that supports only 2 buttons and can be used for yes/no answers */
 	MODAL_FORM: "modal",
 });
