456409a270c4bbfd0b2c34a560069340e0dbde2e
kotinash <fylhsq102030+github@gmail.com>
2023-09-01 21:47:15 +0200

    `package.json`: Removed `center-align` dependency

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const crashFileName = `./crash-reports/server-crash-${Math.floor(Math.random() *
  */
 async function createConfigFilesAndDebug() {
 	if (!fs.existsSync("config.yml")) {
-		const configPath = path.join(__dirname, process.env.TEST ? ".." : ".", "src", "resources", "defaultConfig.yml");
+		const configPath = path.join(__dirname, ".", "src", "resources", "defaultConfig.yml");
 		const config = fs.readFileSync(configPath);
 
 		fs.writeFileSync("config.yml", config);
