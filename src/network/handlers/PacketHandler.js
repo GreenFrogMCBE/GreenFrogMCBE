274dc7bcc3148a96055d151c3eefe9f6a873717a
kotinash <fylhsq102030+github@gmail.com>
2023-10-06 19:34:50 +0300

    PacketHandler: Formatting

--- src/network/handlers/PacketHandler.js
+++ src/network/handlers/PacketHandler.js
@@ -141,7 +141,9 @@ class PacketHandler {
 	 * @returns {PacketHandlingException} - The rate limit exception.
 	 */
 	createRateLimitException(player) {
-		const exceptionMessage = getKey("exceptions.network.rateLimited").replace("%s", player.username).replace("%d", player.network.packetCount.toString());
+		const exceptionMessage = getKey("exceptions.network.rateLimited")
+			.replace("%s", player.username)
+			.replace("%d", player.network.packetCount);
 
 		return new PacketHandlingException(exceptionMessage);
 	}
@@ -232,7 +234,10 @@ class PacketHandler {
 	 * @param {Error} error
 	 */
 	handlePacketError(player, error) {
-		Logger.error(getKey("exceptions.network.packetHandlingError").replace("%s", player.username).replace("%d", error.stack || ""));
+		Logger.error(
+			getKey("exceptions.network.packetHandlingError")
+				.replace("%s", player.username)
+				.replace("%d", error.stack));
 
 		try {
 			player.kick(getKey("kickMessages.invalidPacket"));
