cef36f92f10d5e1ae0a21a0791523d76b1b2d243
kotinash <fylhsq102030+github@gmail.com>
2024-02-02 16:05:54 +0100

    cleaned up `ServerFormRequestPacket`

--- src/network/packets/ServerFormRequestPacket.js
+++ src/network/packets/ServerFormRequestPacket.js
@@ -41,28 +41,17 @@ class ServerFormRequestPacket extends Packet {
 	 * @param {import("Frog").Player} player
 	 */
 	write_packet(player) {
-		let data
-
-		if (this.type === Form.MODAL_FORM) {
-			data = {
-				content: this.content,
-				button1: this.button1,
-				button2: this.button2,
-				title: this.title,
-				type: this.type,
-			}
-		} else {
-			data = {
-				content: this.content,
-				buttons: this.buttons,
-				title: this.title,
-				type: this.type,
-			}
-		}
-
 		player.queue(this.name, {
 			form_id: this.id,
-			data: JSON.stringify(data),
+			data: JSON.stringify({
+				content: this.content,
+				title: this.title,
+				type: this.type,
+				...(this.type === Form.MODAL_FORM
+					? { button1: this.button1, button2: this.button2 }
+					: { buttons: this.buttons }
+				),
+			}),
 		})
 	}
 }
